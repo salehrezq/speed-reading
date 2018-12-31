7542ed9a038809eb00cc6ff9202cc38ac8ec8b54
Saleh Rezq <salehrezq@gmail.com>
2018-12-31 03:47:41 +0200

    Fix package name typo

--- src/speedreading/SpeadReading.java
+++ src/speedreading/SpeadReading.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package speadreading;
+package speedreading;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
